b605f4848844d4e0cb3886c8a41ab38157b7b110
Eric Poppleton <Vehin77@hotmail.com>
2020-02-12 17:30:25 -0700

    Add project files.

---
+++ Troiamis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Troiamis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
