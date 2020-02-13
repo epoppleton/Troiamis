dbeee191c08aa246929ab1b696ca375911166c37
Ahouston429 <ahouston@student.neumont.edu>
2020-02-12 18:00:50 -0700

    new branch init commit

--- Troiamis/Startup.cs
+++ Troiamis/Startup.cs
@@ -17,6 +17,8 @@ namespace Troiamis
             Configuration = configuration;
         }
 
+        //INIT for a new branch
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
