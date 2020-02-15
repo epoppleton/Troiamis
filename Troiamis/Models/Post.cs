8b886602b60970c79e7cf46b4b3697e86fba3ba2
Hunter Lemley <hlemley@student.neumont.edu>
2020-02-14 19:06:38 -0700

    Add model classes: Post, ImagePost, EmbedPost, PostsGallery and User

---
+++ Troiamis/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Troiamis.Models
+{
+    public class Post
+    {
+        string fileName { get; set; }
+        string posterName { get; set; }
+        string postTitle { get; set; }
+        string postContent { get; set; }
+        DateTime timeStamp { get; set; }
+        int ratings { get; set; }
+    }
+}
---
+++ Troiamis/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Troiamis.Models
+{
+    public class User
+    {
+        string userEmail { get; set; }
+        int age { get; set; }
+        string avatarImageString { get; set; }
+    }
+}
