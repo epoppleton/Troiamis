51f67817564ef4196aa00d23020b7fb1138e3927
Eric Poppleton <Vehin77@hotmail.com>
2020-02-28 17:30:34 -0700

    Create branch

--- Troiamis/Controllers/HomeController.cs
+++ Troiamis/Controllers/HomeController.cs
@@ -62,7 +62,7 @@ namespace Troiamis.Controllers
             ModelsCombined.User user = DB.Users.Where(u => u.userName == Request.Form["userName"] && u.password == Request.Form["password"]).FirstOrDefault();
             Debug.WriteLine(Request.Form["userName"].ToString());
             Debug.WriteLine(Request.Form["password"].ToString());
-
+            
             if (user != null)
             {
                 HttpContext.Session.SetString("username", Request.Form["userName"].ToString());
