fc4d9336e676a1edf9258cfb355d25c529b2e998
Ahouston429 <ahouston@student.neumont.edu>
2020-02-28 18:53:01 -0700

    Make a few of the basic routing statements

--- Troiamis/Startup.cs
+++ Troiamis/Startup.cs
@@ -4,11 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Troiamis.ModelsCombined;
 //test
 namespace Troiamis
 {
@@ -26,36 +24,35 @@ namespace Troiamis
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TroiamisDBContext>(opt => opt.UseSqlServer(Configuration["ConnectionStrings:cdb_conn"]));
             services.AddControllersWithViews();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
             app.UseStaticFiles();
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "Home",
+                    template: "{controller=Home}/{action=Index}");
+
+                routes.MapRoute(
+                    name: "Login",
+                    template: "Login",
+                    defaults: new { Controller = "Home", action = "Login" });
+
+                //routes.MapRoute(
+                //    name: "",
+                //    template: "{}",
+                //    defaults: new { Controller =  });
+
             });
         }
     }
 }
-
-//Hunter wuz here
